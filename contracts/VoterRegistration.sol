2b47b76411e8254fab8b069de65ad3db929a815e
Eugene Korpan <eugene@active-bridge.com>
2018-10-11 13:19:56 +0300

    Only owner can add a voter

--- contracts/VoterRegistration.sol
+++ contracts/VoterRegistration.sol
@@ -4,11 +4,22 @@ contract VoterRegistration {
 
   mapping(address => bool) registered;
 
+  address public owner;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
   function isRegisterd(address voter) view public returns (bool) {
     return registered[voter];
   }
 
-  function register(address voter) public {
+  function register(address voter) public onlyOwner {
     registered[voter] = true;
   }
 }
--- test/VoterRegistration.js
+++ test/VoterRegistration.js
@@ -4,6 +4,7 @@ contract("VoterRegistration", async (accounts) => {
 
   it("can check whether or not a votar is registered", async () => {
     voterRegistraion = await VoterRegistration.deployed();
+
     let unregisteredVoter = accounts[0];
     var isRegisterd = await voterRegistraion.isRegisterd.call(unregisteredVoter);
 
@@ -14,7 +15,23 @@ contract("VoterRegistration", async (accounts) => {
     var isRegisterd = await voterRegistraion.isRegisterd.call(registeredVoter);
 
     assert.equal(isRegisterd, true, "Voter should be registered.");
+  });
 
-  })
+  it("only lets the owner register new voters", async () => {
+    voterRegistraion = await VoterRegistration.deployed();
+
+    let voter = accounts[7];
+
+    try {
+      await voterRegistraion.register(voter, { from: accounts[4] });
+    } catch(err) {
+      var isRegisterd = await voterRegistraion.isRegisterd.call(voter);
+      assert.equal(isRegisterd, false, "Voter should not be registered.");
+    }
+
+    await voterRegistraion.register(voter, { from: accounts[0] });
+    var isRegisterd = await voterRegistraion.isRegisterd.call(voter);
+    assert.equal(isRegisterd, true, "Voter should be registered.");
+  });
 
 })
